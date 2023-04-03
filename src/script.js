66e968823dd5fb28c0857f37bfac069fa4c32ebc
Talilo Tarlison <101112484+talilotarlison@users.noreply.github.com>
2023-04-03 10:06:05 -0300

    Add files via upload

--- src/script.js
+++ src/script.js
@@ -7,14 +7,15 @@ method:"GET"
 });
 const data = await res.json();
   
-console.log(data);
-  
+
 var link_favorito = document.getElementById("marcadores_pasterbin");
-  
+
+
 
 for ( var i=0 ; i < data.length; i++){
+  var dateAdd = new Date();	
   var titulo = document.createElement("p");
-  titulo.innerHTML += `<img src="https://png.pngtree.com/element_pic/16/11/19/3bcc8db55bdecafcd59753753a4f0aa9.jpg" alt="Favoritos" width="15" height="15"><a href="${data[i].url}" target="_blank">${data[i].nome}<font color="green">- pasterbin.com</font> - [PASTERBIN] <font color="black">adicionado em 24 de mar. de 2023</font></a>` ;
+  titulo.innerHTML += `<img src="https://png.pngtree.com/element_pic/16/11/19/3bcc8db55bdecafcd59753753a4f0aa9.jpg" alt="Favoritos" width="15" height="15"><a href="${data[i].url}" target="_blank">${data[i].nome}<font color="green">- pasterbin.com</font> - [PASTERBIN] <font color="black">adicionado em ${dateAdd}</font></a>` ;
   link_favorito.appendChild(titulo);
 }
 }
